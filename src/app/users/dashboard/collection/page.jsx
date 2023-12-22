6de84c28c84ae40393f023f62014b61bff0fd549
TRakryan7 <Trakryan7@gmail.com>
2023-12-22 23:08:38 +0700

    adding my collection page

--- src/app/users/dashboard/collection/page.jsx
+++ src/app/users/dashboard/collection/page.jsx
@@ -1,25 +1,35 @@
 import Link from "next/link";
 import Image from "next/image";
+import Header from "@/components/Dashboard/Header";
 
 const Page = () => {
     return (
-        <section className="mt-4">
-            <h3 className="text-2xl text-primary">My Collection</h3>
+        <section className="mt-4 px-4 w-full">
+            <Header title={"My Collection"}/>
             <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-                <Link href="/" className="border-2 border-accent">
-                    <Image src={""} alt="..." width={350} height={350}/>
-                    <div className="w-full">
-                        <h5>Judul Anime Judul Anime</h5>
+                <Link href="/" className="border-2 border-accent relative">
+                    <Image src={""} alt="..." width={350} height={350} className="w-full"/>
+                    <div className="w-full absolute bottom-0 h-16 flex justify-center items-center bg-accent">
+                        <h5 className="text-xl text-center">Judul Anime Judul Anime</h5>
                     </div>
                 </Link>
-                <Link href="/" className="border-2 border-accent">
-                    <Image src={""} alt="..." width={350} height={350}/>
+                <Link href="/" className="border-2 border-accent relative">
+                    <Image src={""} alt="..." width={350} height={350} className="w-full"/>
+                    <div className="w-full absolute bottom-0 h-16 flex justify-center items-center bg-accent">
+                        <h5 className="text-xl text-center">Judul Anime Judul Anime</h5>
+                    </div>
                 </Link>
-                <Link href="/" className="border-2 border-accent">
-                    <Image src={""} alt="..." width={350} height={350}/>
+                <Link href="/" className="border-2 border-accent relative">
+                    <Image src={""} alt="..." width={350} height={350} className="w-full"/>
+                    <div className="w-full absolute bottom-0 h-16 flex justify-center items-center bg-accent">
+                        <h5 className="text-xl text-center">Judul Anime Judul Anime</h5>
+                    </div>
                 </Link>
-                <Link href="/" className="border-2 border-accent">
-                    <Image src={""} alt="..." width={350} height={350}/>
+                <Link href="/" className="border-2 border-accent relative">
+                    <Image src={""} alt="..." width={350} height={350} className="w-full"/>
+                    <div className="w-full absolute bottom-0 h-16 flex justify-center items-center bg-accent">
+                        <h5 className="text-xl text-center">Judul Anime Judul Anime</h5>
+                    </div>
                 </Link>
             </div>
         </section>
